bc2d5d3b81e04ad64310f5486dd1cbf65b804a77
stroJy <>
2021-04-08 20:43:33 +0000

    DONE: All Tests are OKAY ^^ !

--- Palindrome.java
+++ Palindrome.java
@@ -1,7 +1,17 @@
+import java.util.*;
 public class Palindrome{
     public static boolean is_palindrome(String s)
     {
-        // add your code here
-        return true;
+     String inverse="";
+     int i;
+     for(i=(s.length()-1);i>=0;i--) {
+       inverse= inverse+ s.charAt(i);
+     }
+     if(inverse.equals(s)) {
+       return true;
+     }
+     else {
+       return false;
+     }
     }
 }
